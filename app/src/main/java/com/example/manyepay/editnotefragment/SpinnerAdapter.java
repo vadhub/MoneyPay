88d329a9c927dcc4c39e7eb54b0a73b89f83734f
Vadim <gabderahmanov99@gmail.com>
2020-12-16 22:24:26 +0400

    add adapter on spinner

---
+++ app/src/main/java/com/example/manyepay/editnotefragment/SpinnerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.manyepay.editnotefragment;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class SpinnerAdapter extends ArrayAdapter<String> {
+
+    public SpinnerAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View row = convertView;
+        if (row == null)
+            row = super.getView(position, convertView, parent);
+        if (parent.getWidth() > 0)
+        {
+            TextView label = (TextView) row.findViewById(android.R.id.text1);
+            label.setTextSize(TypedValue.COMPLEX_UNIT_PX, );
+            label.setPadding(label.getPaddingLeft(), 0, 0, 0);
+        }
+        return(row);
+    }
+}
